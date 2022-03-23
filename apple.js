ddf9930ec86f27b6df8146c4693eea4056e6378b
Agastya Mishra <83354866+agastyam12@users.noreply.github.com>
2022-03-23 10:51:38 +0530

    Add files via upload

---
+++ apple.js
@@ -0,0 +1,42 @@
+img = "";
+status = "";
+objects = [];
+function preload(){
+    img = loadImage("apple.jpg");
+}
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded !");
+    status = true;
+    objectDetector.detect(img , gotResult);
+}
+
+function gotResult(error  , results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    if(status != undefined){
+        image(img ,0 , 0 , 640 , 420);
+        for(var i = 0; i < objects.length; i++){
+           document.getElementById("status").innerHTML = "Status : objects detected";
+           fill(255 , 0 , 0 );
+           percent = floor(objects[i].confidence * 100);
+           document.getElementById("object").innerHTML = "Confidence : " + percent + "% " + " Object Detected : APPLE";
+           noFill();
+           stroke(255, 0 , 0 );
+           rect( 160 , 70 , 340 , 320);
+           console.log(objects[i].label+" " + percent + "%" + " Object Detected : APPLE" + objects[i].x + 15 , objects[i].y + 15    )
+        }
+      }
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+function appleOpen(){
+    window.location = "apple.html";
+}
+function bookshelfOpen(){
+    window.location = "bookshelf.html";
+}
+function carOpen(){
+    window.location = "car.html";
+}
+function footballOpen(){
+    window.location = "football.html";
+}
+function spongeOpen(){
+    window.location = "sponge.html";
+}
